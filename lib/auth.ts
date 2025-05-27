020956b684059bccc1c943f37304d7061d3b5c04
marciodavid <marciodavid81@gmail.com>
2025-05-27 14:43:30 -0300

    feat: add signup

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,22 +2,27 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/src/db";
+import * as schema from "@/src/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     usePlural: true,
+    schema,
   }),
   user: {
-    modelName: "usersTable"
+    modelName: "usersTable",
   },
   session: {
-    modelName: "sessionsTable"
+    modelName: "sessionsTable",
   },
   account: {
-    modelName: "accountsTable"
+    modelName: "accountsTable",
   },
   verification: {
-    modelName: "verificationsTable"
+    modelName: "verificationsTable",
+  },
+  emailAndPassword: {
+    enabled: true,
   },
 });
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return ( 
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1>Dashboard</h1>
+    </div>
+   );
+}
+ 
+export default DashboardPage;
